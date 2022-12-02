81ac44716bdce630c152498d4af75d6d010eaa3c
dadabaej <djam388@hotmail.com>
2022-12-03 00:48:27 +0500

    Attempt #1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,18 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        boolean isAllPositive = true;
+        for (String arg : args) {
+
+            if (!StringUtils.isPositiveNumber(arg)) {
+                isAllPositive = false;
+            }
+        }
+        return isAllPositive;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,18 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        boolean isPositive = false;
+        try {
+            if (str.matches("\\d*.\\d*")
+                    && !str.equals(" ")
+                    && Double.parseDouble(str) > 0
+                    && !str.startsWith("0")) {
+                isPositive = true;
+            }
+        } catch (NullPointerException e) {
+            return false;
+        }
+
+        return isPositive;
     }
 }
